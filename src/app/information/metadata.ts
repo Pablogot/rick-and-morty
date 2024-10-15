5345c9ec8055457af5b97fc1e952d00c1b84345f
Pablo <pablollanos92@gmail.com>
2024-10-15 22:11:20 +1100

    Add metadata

---
+++ src/app/information/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rick and Morty World",
+  description: "Explore characters from the Rick and Morty!",
+};
---
+++ src/app/metadata.ts
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rick and Morty World",
+  description: "Explore characters from the Rick and Morty universe. Enter your details to get started on your adventure!",
+};
